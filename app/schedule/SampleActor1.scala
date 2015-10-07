91400109a6b1a9d1e98b2a81602ada6d7e8749bc
石津恭輔 <ishizukyousuke@ishidzukyousuke-no-MacBook-Pro.local>
2015-10-08 02:33:44 +0900

    バッチ改修

--- app/schedule/SampleActor1.scala
+++ app/schedule/SampleActor1.scala
@@ -8,7 +8,7 @@ import java.net.URL
 
 import com.sun.syndication.feed.synd.{SyndEntry, SyndFeed}
 import com.sun.syndication.io.{SyndFeedInput, XmlReader}
-import model.NewsStory
+import model.{RSS, NewsStory}
 import play.api.Play.current
 import play.api.libs.json.Json
 import play.api.libs.ws.WS
@@ -19,19 +19,20 @@ class SampleActor1 extends ActorBase {
 
   /** 処理本体 */
   def execute() = {
-    print("取り込みバッチスタート")
-    val url: String = "http://news020.blog13.fc2.com/?xml"
-    val input: SyndFeedInput = new SyndFeedInput
-    val feed: SyndFeed = input.build(new XmlReader(new URL(url)))
-    implicit val univWrites = Json.writes[NewsStory]
+    RSS.findAll().foreach(rss => {
+      val input: SyndFeedInput = new SyndFeedInput
+      val feed: SyndFeed = input.build(new XmlReader(new URL(rss.url)))
+      implicit val univWrites = Json.writes[NewsStory]
 
-    val list: List[NewsStory] = feed.getEntries.map(entry => {
-      val e: SyndEntry = entry.asInstanceOf[SyndEntry]
-      NewsStory(e.getLink, feed.getLink, feed.getTitle, e.getTitle, e.getTitle, e.getDescription.getValue, feed.getTitle,
-        "%tY%<tm%<td" format e.getPublishedDate)
-    }
-    ).toList
-    WS.url("http://localhost:8983/solr/solrbook/update/json?commit=true")
-      .withHeaders("Content-Type" -> "application/json").post(Json.toJson(list))
+      val list: List[NewsStory] = feed.getEntries.map(entry => {
+        val e: SyndEntry = entry.asInstanceOf[SyndEntry]
+        NewsStory(e.getLink, feed.getLink, feed.getTitle, e.getTitle, e.getTitle, e.getDescription.getValue, feed.getTitle,
+          "%tY%<tm%<td" format e.getPublishedDate)
+      }
+      ).toList
+      WS.url("http://localhost:8983/solr/solrbook/update/json?commit=true")
+        .withHeaders("Content-Type" -> "application/json").post(Json.toJson(list))
+    })
+
   }
 }
